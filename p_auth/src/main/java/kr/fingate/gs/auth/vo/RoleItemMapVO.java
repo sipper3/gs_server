a84508e59ff5319ed8faed1180e4d13582829537
shan <shan@fingate.kr>
2023-09-26 19:01:57 +0900

    role 테스트 수정

--- p_auth/src/main/java/kr/fingate/gs/auth/vo/RoleItemMapVO.java
+++ p_auth/src/main/java/kr/fingate/gs/auth/vo/RoleItemMapVO.java
@@ -1,12 +1,14 @@
 package kr.fingate.gs.auth.vo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import kr.fingate.gs.comon.vo.BaseVO;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class RoleItemMapVO extends BaseVO {
     //역할번호
     private long roleNo;
