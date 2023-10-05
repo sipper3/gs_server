283d837188a37e518a7644a330d100f925acba67
alex <alex@fingate.kr>
2023-10-05 16:19:06 +0900

    TESTCASE NOT USE YET

--- p_auth/src/test/java/kr/fingate/gs/auth/login/controller/LoginPublicControllerTest.java
+++ p_auth/src/test/java/kr/fingate/gs/auth/login/controller/LoginPublicControllerTest.java
@@ -30,37 +30,37 @@ import static org.springframework.restdocs.payload.JsonFieldType.*;
 @SpringBootTest(classes = {AuthApplication.class})
 public class LoginPublicControllerTest extends TestBaseController {
 
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    @Test
-    @DisplayName("토큰발급")
-    void testGetToken() throws Exception {
-
-        //given
-
-        Item responseItems = field(
-            field("token", STRING, "토큰", Item.State.OPTIONAL)
-        );
-//        List<FieldDescriptor> requestFields = generateFieldDescriptor(searchRoleDto, "searchRoleName", "searchRoleNo");
-
-        //when
-        ResultActions resultActions = mockMvc.perform(
-                post("/auth/api/sso/getToken")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(searchRoleDto))
-        );
-        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
-
-        generateDocument(resultActions,
-                ResourceSnippetParameters.builder()
-                        .tag("롤조회") // 문서에 표시될 태그.summary("사용자 정보 생성") // 문서에 표시될 요약정보
-                        .description("개별롤에 대한 롤 조회") // 문서에 표시될 상세정보
-                        .requestSchema(schema("SearchRoleDto"))
-//                        .requestFields(requestFields)
-                        .responseFields(responseItems.toFields())
-                        .build(), true);
-    }
+//    @Autowired
+//    protected ObjectMapper objectMapper;
+//
+//    @Test
+//    @DisplayName("토큰발급")
+//    void testGetToken() throws Exception {
+//
+//        //given
+//
+//        Item responseItems = field(
+//            field("token", STRING, "토큰", Item.State.OPTIONAL)
+//        );
+////        List<FieldDescriptor> requestFields = generateFieldDescriptor(searchRoleDto, "searchRoleName", "searchRoleNo");
+//
+//        //when
+//        ResultActions resultActions = mockMvc.perform(
+//                post("/auth/api/sso/getToken")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON)
+////                .content(objectMapper.writeValueAsString(searchRoleDto))
+//        );
+//        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+//
+//        generateDocument(resultActions,
+//                ResourceSnippetParameters.builder()
+//                        .tag("롤조회") // 문서에 표시될 태그.summary("사용자 정보 생성") // 문서에 표시될 요약정보
+//                        .description("개별롤에 대한 롤 조회") // 문서에 표시될 상세정보
+//                        .requestSchema(schema("SearchRoleDto"))
+////                        .requestFields(requestFields)
+//                        .responseFields(responseItems.toFields())
+//                        .build(), true);
+//    }
 
 }
