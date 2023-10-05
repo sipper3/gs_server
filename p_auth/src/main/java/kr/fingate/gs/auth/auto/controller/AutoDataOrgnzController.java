6c6f11ab9fdf7845cb15f31b2413e69e2248ea9b
alex <alex@fingate.kr>
2023-10-05 17:27:16 +0900

    데이터 열람조직 빈 Controller

---
+++ p_auth/src/main/java/kr/fingate/gs/auth/auto/controller/AutoDataOrgnzController.java
@@ -0,0 +1,11 @@
+package kr.fingate.gs.auth.auto.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/${project.name}/api/auto/orgnz")
+@RestController
+@RequiredArgsConstructor
+public class AutoDataOrgnzController {
+}
