851df1fdd9d3167c7b1ff06a66f1d60a86cd61bc
swkim <swkim@fingate.kr>
2023-10-05 18:47:48 +0900

    암호화/복호화

--- common/src/main/java/kr/fingate/gs/comon/util/EncryptionUtil.java
+++ common/src/main/java/kr/fingate/gs/comon/util/EncryptionUtil.java
@@ -4,16 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.spec.AlgorithmParameterSpec;
 
 /**
@@ -30,15 +24,15 @@ public class EncryptionUtil {
 	private static String KEY = "20A3B00A12H397E1228C2233J33D311C";
 	private static String IV = "sDaOgZhI4412lkJb";
 
-	public static String aesEncode(String str) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	public static String aesEncode(String str) throws Exception {
 		return EncryptionUtil.aesEncode(str, EncryptionUtil.KEY);
 	}
 
-	public static String aesDecode(String str) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	public static String aesDecode(String str) throws Exception {
 		return EncryptionUtil.aesDecode(str, EncryptionUtil.KEY);
 	}
 
-	public static String aesEncode(String str, String key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	public static String aesEncode(String str, String key) throws Exception {
 		if(StringUtils.isEmpty(str)) return "";
 
 		byte[] textBytes = str.getBytes(StandardCharsets.UTF_8);
@@ -60,7 +54,7 @@ public class EncryptionUtil {
 
 	}
 
-	public static String aesDecode(String str, String key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	public static String aesDecode(String str, String key) throws Exception {
 
 		if(StringUtils.isEmpty(str)) return "";
 
@@ -82,63 +76,14 @@ public class EncryptionUtil {
 		return new String(cipher.doFinal(textBytes), StandardCharsets.UTF_8);
 	}
 
-
-	private static String charEnMapping(String enStr) {
-
-		enStr = enStr.replaceAll("[/]", "_slash_");
-		enStr = enStr.replaceAll("[+]", "_plus_");
-		enStr = enStr.replaceAll("[:]", "_col_");
-		enStr = enStr.replaceAll("[;]", "_smcol_");
-		enStr = enStr.replaceAll("[&]", "_amper_");
-		enStr = enStr.replaceAll("[$]", "_dol_");
-		enStr = enStr.replaceAll("[']", "_grave_");
-		enStr = enStr.replaceAll("[~]", "_tilde_");
-		enStr = enStr.replaceAll("[!]", "_exmark_");
-		enStr = enStr.replaceAll("[@]", "_at_");
-		enStr = enStr.replaceAll("[#]", "_sharp_");
-		enStr = enStr.replaceAll("[%]", "_per_");
-		enStr = enStr.replaceAll("[*]", "_ast_");
-		enStr = enStr.replaceAll("[\"]", "_quot_");
-		enStr = enStr.replaceAll("[']", "_apost_");
-		enStr = enStr.replaceAll("[?]", "_quest_");
-		enStr = enStr.replaceAll("[-]", "_min_");
-		enStr = enStr.replaceAll("[=]", "_eq_");
-
-		return enStr;
-	}
-
-	private static String charDeMapping(String enStr) {
-
-		enStr = enStr.replaceAll("_slash_", "/");
-		enStr = enStr.replaceAll("_plus_", "+");
-		enStr = enStr.replaceAll("_col_", ":");
-		enStr = enStr.replaceAll("_smcol_", ";");
-		enStr = enStr.replaceAll("_amper_", "&");
-		enStr = enStr.replaceAll("_dol_", "$");
-		enStr = enStr.replaceAll("_grave_", "'");
-		enStr = enStr.replaceAll("_tilde_", "~");
-		enStr = enStr.replaceAll("_exmark_", "!");
-		enStr = enStr.replaceAll("_at_", "@");
-		enStr = enStr.replaceAll("_sharp_", "#");
-		enStr = enStr.replaceAll("_per_", "%");
-		enStr = enStr.replaceAll("_ast_", "*");
-		enStr = enStr.replaceAll("_quot_", "\"");
-		enStr = enStr.replaceAll("_apost_", "'");
-		enStr = enStr.replaceAll("_quest_", "?");
-		enStr = enStr.replaceAll("_min_", "-");
-		enStr = enStr.replaceAll("_eq_", "=");
-
-		return enStr;
-	}
-
-	public static void main(String[] args) {
-		try {
-			log.info(EncryptionUtil.aesEncode("TEST1123232323223232323"));
-			log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-			log.info(EncryptionUtil.aesDecode("G85W4XefOqKNt2CwbGTHpi+x5p2Io29ttOBzC+26dlM="));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public static void main(String[] args) {
+//		try {
+//			log.info(EncryptionUtil.aesEncode("TEST1123232323223232323"));
+//			log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+//			log.info(EncryptionUtil.aesDecode("G85W4XefOqKNt2CwbGTHpi+x5p2Io29ttOBzC+26dlM="));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 }
