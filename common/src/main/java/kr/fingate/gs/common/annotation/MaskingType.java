fc900c19cf46388d249b882d66fe989ff800c435
shan <shan@fingate.kr>
2023-10-06 18:38:40 +0900

    masking 및 암복호화 관련 mybatis interceptor 추가

---
+++ common/src/main/java/kr/fingate/gs/common/annotation/MaskingType.java
@@ -0,0 +1,16 @@
+package kr.fingate.gs.common.annotation;
+
+public enum MaskingType {
+
+	NAME,
+	EMAIL,
+	IDNTN, /* 주민번호 */
+	LINE_TEL, /* 유선전화 */
+	TEL,
+	CARD,
+	ACCOUNT,
+	ADRES /* 주소 */
+	;
+
+}
+
--- common/src/main/java/kr/fingate/gs/common/mybatis/PrivateInfoInterceptor.java
+++ common/src/main/java/kr/fingate/gs/common/mybatis/PrivateInfoInterceptor.java
@@ -1,7 +1,6 @@
 package kr.fingate.gs.common.mybatis;
 
-import kr.fingate.gs.common.annotation.Masking;
-import kr.fingate.gs.common.annotation.Private;
+import kr.fingate.gs.common.annotation.*;
 import kr.fingate.gs.common.util.EncryptionUtil;
 import kr.fingate.gs.common.util.MaskingUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -16,74 +15,118 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
-@Intercepts(@Signature(
-        type= Executor.class,
-        method="query",
-        args= {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}))
+@Intercepts({
+        @Signature(
+                type= Executor.class,
+                method="query",
+                args= {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(
+                type= Executor.class,
+                method="update",
+                args= {MappedStatement.class, Object.class})
+})
 public class PrivateInfoInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        Object resultSet = invocation.proceed();
-        if(resultSet != null) {
-            List<?> list = null;
-            if(resultSet instanceof List) {
-                list = (List<?>)resultSet;
-                for(Object obj : list) {
-                    privateInfoMark(obj);
+        String methodName = invocation.getMethod().getName();
+        try {
+            /* dao method에 Encrypt annotation이 있는지 체크하여 annotation이 있는 경우에만 암호화 여부 체크 */
+            Object[] args = invocation.getArgs();
+            MappedStatement mapped = (MappedStatement) args[0];
+            String mappedId = mapped.getId();
+            String className = mappedId.substring(0, mappedId.lastIndexOf("."));
+            String daoMethodName = mappedId.substring(mappedId.lastIndexOf(".") + 1);
+
+            log.debug("className : {}", className);
+            log.debug("methodName : {}", daoMethodName);
+            Class c = Class.forName(className);
+            Method[] methods = c.getDeclaredMethods();
+            Method daoMethod = Arrays.stream(methods).filter(method1 -> method1.getName().equals(daoMethodName)).findAny().orElse(null);
+
+            assert daoMethod != null;
+            PrivateSql privateSqlAn = daoMethod.getDeclaredAnnotation(PrivateSql.class);
+            if(!(privateSqlAn == null || (!privateSqlAn.masking() && !privateSqlAn.encrypt() && !privateSqlAn.decrypt()))) {
+                if(methodName.equals("update") && privateSqlAn.encrypt()) {
+                    Object param = args[1];
+                    encrypt(param);
+                }else if(methodName.equals("query")) {
+                    Object resultSet = invocation.proceed();
+                    if(resultSet != null) {
+                        if(resultSet instanceof List<?> list) {
+                            for(Object obj : list) {
+                                privateInfoMark(obj);
+                            }
+                        } else {
+                            privateInfoMark(resultSet);
+                        }
+                    }
+                    return resultSet;
                 }
-
-            } else {
-                privateInfoMark(resultSet);
             }
-        }
 
-        return resultSet;
+        }catch (Exception e) {
+            log.error("", e);
+        }
+        return invocation.proceed();
     }
 
-    private void privateInfoMark(Object obj) throws Exception {
+    private void privateInfoMark(Object obj) {
 
         Field[] fields = getFields(obj.getClass());
 
         Arrays.stream(fields)
-                .filter(f -> f.getDeclaredAnnotation(Private.class) != null)
                 .forEach(f -> {
-                    Private piAn = f.getDeclaredAnnotation(Private.class);
                     if(String.class.equals(f.getType())) {
 
                         f.setAccessible(true);
 
                         try {
                             String value = "";
+
                             if(f.get(obj) != null && !f.get(obj).equals("")) {
 
                                 value = (String)f.get(obj);
 
-                                if(piAn.encrypt()){
+                                /* 복호화 annotation이 있으면 복호화 처리 */
+                                Decrypt decryptAn = f.getDeclaredAnnotation(Decrypt.class);
+                                if(decryptAn != null && decryptAn.value()){
                                     value = EncryptionUtil.aesDecode(value);
                                 }
 
-                                if (piAn.value() == Masking.IDNTN) {
-                                    value = MaskingUtil.maskIdntn(value);
-                                } else if (piAn.value() == Masking.TEL) {
-                                    value = MaskingUtil.maskTel(value);
-                                } else if (piAn.value() == Masking.LINE_TEL) {
-                                    value = MaskingUtil.maskTel(value);
-                                } else if (piAn.value() == Masking.NAME) {
-                                    value = MaskingUtil.maskName(value);
-                                } else if (piAn.value() == Masking.CARD) {
-                                    value = MaskingUtil.maskCard(value);
-                                } else if (piAn.value() == Masking.ACCOUNT) {
-                                    value = MaskingUtil.maskAccount(value);
-                                } else if(piAn.value() == Masking.EMAIL) {
-                                    value = MaskingUtil.maskEmail(value);
-                                } else if (piAn.value() == Masking.ADRES) {
-                                    value = MaskingUtil.maskAdres(value);
+                                /* 암호화 annotation이 있으면 암호화 처리 */
+                                Encrypt encryptAn = f.getDeclaredAnnotation(Encrypt.class);
+                                if(encryptAn != null && encryptAn.value()){
+                                    value = EncryptionUtil.aesEncode(value);
                                 }
 
+                                /* Masking annotation이 있으면 Masking 처리 */
+                                Masking piAn = f.getDeclaredAnnotation(Masking.class);
+                                if(piAn == null) {
+                                    return;
+                                }
+
+                                if (piAn.value() == MaskingType.IDNTN) {
+                                    value = MaskingUtil.maskIdntn(value);
+                                } else if (piAn.value() == MaskingType.TEL) {
+                                    value = MaskingUtil.maskTel(value);
+                                } else if (piAn.value() == MaskingType.LINE_TEL) {
+                                    value = MaskingUtil.maskTel(value);
+                                } else if (piAn.value() == MaskingType.NAME) {
+                                    value = MaskingUtil.maskName(value);
+                                } else if (piAn.value() == MaskingType.CARD) {
+                                    value = MaskingUtil.maskCard(value);
+                                } else if (piAn.value() == MaskingType.ACCOUNT) {
+                                    value = MaskingUtil.maskAccount(value);
+                                } else if(piAn.value() == MaskingType.EMAIL) {
+                                    value = MaskingUtil.maskEmail(value);
+                                } else if (piAn.value() == MaskingType.ADRES) {
+                                    value = MaskingUtil.maskAdres(value);
+                                }
                             }
 
                             f.set(obj, value);
@@ -95,7 +138,27 @@ public class PrivateInfoInterceptor implements Interceptor {
                             log.error(e.getMessage());
                         }
                     }
+                });
+    }
 
+    private void encrypt(Object obj) {
+
+        Field[] fields = getFields(obj.getClass());
+
+        Arrays.stream(fields)
+                .filter(f -> f.getDeclaredAnnotation(Encrypt.class) != null && String.class.equals(f.getType()))
+                .forEach(f -> {
+                    f.setAccessible(true);
+                    try {
+                        String value = "";
+                        if(f.get(obj) != null) {
+                            value = EncryptionUtil.aesEncode((String) f.get(obj));
+                            f.set(obj, value);
+                        }
+
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    }
                 });
     }
 
