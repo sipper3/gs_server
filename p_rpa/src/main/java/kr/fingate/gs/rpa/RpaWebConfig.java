f0bcaf17bbc03613ca0c7e2e1b8d0a56be6cf378
swkim <swkim@fingate.kr>
2023-10-10 14:23:03 +0900

    rpa/core

--- p_rpa/src/main/java/kr/fingate/gs/rpa/RpaWebConfig.java
+++ p_rpa/src/main/java/kr/fingate/gs/rpa/RpaWebConfig.java
@@ -1,17 +1,37 @@
 package kr.fingate.gs.rpa;
 
 import kr.fingate.gs.core.config.WebConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class RpaWebConfig extends WebConfig {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처
-                .allowedMethods("GET", "POST", "HEAD", "OPTIONS") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*") // 허용할 출처
+//                .allowedMethods("GET", "POST", "OPTIONS") // 허용할 HTTP method
+//                .allowCredentials(true) // 쿠키 인증 요청 허용
+//                .maxAge(3000); // 원하는 시간만큼 pre-flight request를 캐싱
+//    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+        config.setMaxAge(3000L);
+
+        source.registerCorsConfiguration("/**", config) ;
+        return new CorsFilter(source);
     }
 }
