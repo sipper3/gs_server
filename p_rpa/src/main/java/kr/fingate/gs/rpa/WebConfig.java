f21a825cfbd75c0fd60bac63b87868d367be9f31
jsan73 <ajs007@gmail.com>
2023-10-10 12:33:13 +0900

    rpa job 설정

--- p_rpa/src/main/java/kr/fingate/gs/rpa/WebConfig.java
+++ p_rpa/src/main/java/kr/fingate/gs/rpa/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*") // 허용할 출처
-                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedMethods("GET", "POST", "HEAD") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
