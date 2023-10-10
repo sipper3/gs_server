59510b87c62a208a2dcc0516fffd0953ee2084bc
swkim <swkim@fingate.kr>
2023-10-10 14:39:00 +0900

    webconfig

--- p_rpa/src/main/java/kr/fingate/gs/rpa/RpaWebConfig.java
+++ p_rpa/src/main/java/kr/fingate/gs/rpa/RpaWebConfig.java
@@ -1,37 +1,34 @@
 package kr.fingate.gs.rpa;
 
 import kr.fingate.gs.core.config.WebConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class RpaWebConfig extends WebConfig {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*") // 허용할 출처
-//                .allowedMethods("GET", "POST", "OPTIONS") // 허용할 HTTP method
-//                .allowCredentials(true) // 쿠키 인증 요청 허용
-//                .maxAge(3000); // 원하는 시간만큼 pre-flight request를 캐싱
-//    }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("http://rpa-local.fingate.kr:8000");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod(HttpMethod.GET);
-        config.addAllowedMethod(HttpMethod.POST);
-        config.addAllowedMethod(HttpMethod.OPTIONS);
-        config.setMaxAge(3000L);
-
-        source.registerCorsConfiguration("/**", config) ;
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 허용할 출처
+                .allowedMethods("GET", "POST", "OPTIONS") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight request를 캐싱
     }
+
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOriginPattern("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod(HttpMethod.GET);
+//        config.addAllowedMethod(HttpMethod.POST);
+//        config.addAllowedMethod(HttpMethod.OPTIONS);
+//        config.setMaxAge(3000L);
+//
+//        source.registerCorsConfiguration("/**", config) ;
+//        return new CorsFilter(source);
+//    }
 }
