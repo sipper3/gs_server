021782c0d17502e37c56646142022b6f24b5faa4
jsan73 <ajs007@gmail.com>
2023-10-10 11:23:28 +0900

    rpa job 설정

--- core/src/main/java/kr/fingate/gs/core/config/WebConfig.java
+++ core/src/main/java/kr/fingate/gs/core/config/WebConfig.java
@@ -69,7 +69,6 @@ public class WebConfig extends WebMvcConfigurationSupport {
 		return resolver;
 	}
 
-	@Profile("local")
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
