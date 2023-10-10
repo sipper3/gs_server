137c03cac6c5c7d9277b42ed23cce207560252fc
swkim <swkim@fingate.kr>
2023-10-10 14:44:47 +0900

    webconfig

--- p_rpa/src/main/java/kr/fingate/gs/rpa/RpaWebConfig.java
+++ p_rpa/src/main/java/kr/fingate/gs/rpa/RpaWebConfig.java
@@ -2,19 +2,18 @@ package kr.fingate.gs.rpa;
 
 import kr.fingate.gs.core.config.WebConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class RpaWebConfig extends WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처
-                .allowedMethods("GET", "POST", "OPTIONS") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000); // 원하는 시간만큼 pre-flight request를 캐싱
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*") // 허용할 출처
+//                .allowedMethods("GET", "POST", "OPTIONS") // 허용할 HTTP method
+//                .allowCredentials(true) // 쿠키 인증 요청 허용
+//                .maxAge(3000); // 원하는 시간만큼 pre-flight request를 캐싱
+//    }
 
 //    @Bean
 //    public CorsFilter corsFilter() {
