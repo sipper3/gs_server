095fbf8317ccec323a244093ab057c6bf1a26119
swkim <swkim@fingate.kr>
2023-10-12 09:19:39 +0900

    암호화 키 조회

--- common/src/main/java/kr/fingate/gs/common/encryption/service/EncryptionService.java
+++ common/src/main/java/kr/fingate/gs/common/encryption/service/EncryptionService.java
@@ -3,8 +3,11 @@ package kr.fingate.gs.common.encryption.service;
 import kr.fingate.gs.common.encryption.dao.EncryptionDao;
 import kr.fingate.gs.common.vo.AesKeyVO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EncryptionService {
@@ -12,6 +15,20 @@ public class EncryptionService {
     final EncryptionDao encryptionDao;
 
     public AesKeyVO getAeskey() throws Exception {
-        return encryptionDao.getAeskey();
+        AesKeyVO aesKeyVO = new AesKeyVO();
+
+        try {
+            aesKeyVO = encryptionDao.getAeskeyM();
+        } catch (Exception ssee) {
+            aesKeyVO = encryptionDao.getAeskeyO();
+        }
+
+        if(aesKeyVO.getAkey().length() > 32) {
+            byte[] bytes = Hex.decodeHex(aesKeyVO.getAkey().toCharArray());
+            aesKeyVO.setAkey(new String(bytes, "UTF-8"));
+            // 3230413342303041313248333937453132323843323233334A33334433313143
+        }
+
+        return aesKeyVO;
     }
 }
